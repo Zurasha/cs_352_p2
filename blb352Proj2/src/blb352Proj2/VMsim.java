38acdfc19faa4ffc82d165d4a0c19c523114dfc4
blb <blb@iastate.edu>
2016-11-29 23:17:47 -0600

    changed stuff

--- blb352Proj2/src/blb352Proj2/VMsim.java
+++ blb352Proj2/src/blb352Proj2/VMsim.java
@@ -1,5 +1,11 @@
 package blb352Proj2;
 
 public class VMsim {
-	
+	// 0 : page/frame size; 1: pages for the process; 2: number of frames in main memory; 3: user processes
+	public static void main (String[] args) {
+		int frameSize = Integer.parseInt(args[0]);
+		int pageCount = Integer.parseInt(args[1]);
+		int memoryFrameCount = Integer.parseInt(args[2]);
+		int userProcessCount = Integer.parseInt(args[3]);
+	}
 }
